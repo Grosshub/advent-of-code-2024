35b2e086153eb4dead1ba9f34b54536dd967f28c
Alexey Gross <alexey.gross@icloud.com>
2024-12-03 00:03:41 +0100

    day01 part2

--- aoc-2024/aoc-2024/Sources/Day01.swift
+++ aoc-2024/aoc-2024/Sources/Day01.swift
@@ -20,7 +20,22 @@ struct Day01: DayExecutable {
     }
 
     static func runPart2(_ input: InputProviding) -> DayResult {
-        // Part 2: write logic here
-        return .error(.notImplemented)
+        let (left, right) = input.raw
+            .split(separator: "\n")
+            .reduce(into: ([Int](), [Int]())) { result, line in
+                let numbers = line
+                    .split(separator: " ")
+                    .compactMap { Int($0) }
+                result.0.append(numbers[0])
+                result.1.append(numbers[1])
+            }
+
+        let rightFrequency = right.reduce(into: [:]) { counts, num in
+            counts[num, default: 0] += 1
+        }
+        let result = left.reduce(0) { total, num in
+            total + (num * rightFrequency[num, default: 0])
+        }
+        return .integer(result)
     }
 }
